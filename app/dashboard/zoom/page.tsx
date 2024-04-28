8aa9abe8a3aac357ef1f9fa740e38546a86316aa
albertescote <albertescote03@gmail.com>
2024-04-28 10:42:44 +0200

    feat: add launch zoom tab

---
+++ app/dashboard/zoom/page.tsx
@@ -0,0 +1,13 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Launch Zoom Meeting',
+};
+
+export default async function Page() {
+  return (
+    <main>
+      <h1>Hola! Bienvendio a Zoom Launch Page</h1>
+    </main>
+  );
+}
